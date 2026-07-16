75ecaf9bcd6bc912dcfed6e8616c274f70437936
Fatima-QA <camalovafatime2003@gmail.com>
2026-07-16 15:16:12 +0400

    2-yə vurma cədvəli

---
+++ src/Task8_Tapsirig2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Task8_Tapsirig2 {
+    public static void main(String[] args){
+
+        System.out.println("--------- 2-yə vurma cədvəli ----------");
+
+        int reqem = 1;
+
+        do {
+            System.out.println("2 x " + reqem + " = " + (2 * reqem));
+            reqem++;
+        } while (reqem <= 10);
+
+
+
+
+    }
+}
