d5a5e93582f8f722e00b1f6f3e182a077698084c
Fatima-QA <camalovafatime2003@gmail.com>
2026-07-16 15:02:33 +0400

    Sayğac

---
+++ src/Task8_Tapsirig1.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Task8_Tapsirig1 {
+    public static void main(String[] args){
+
+        System.out.println("------ Rəqəmlərin ardıcıllıqla düzülüşü ------");
+        int reqem = 1;
+       // Əgər rəqəmdirsə davam eet və sayı bir bir artır
+        do{
+            System.out.println(reqem);
+            reqem++;
+        } while (reqem <= 10); // rəqəm 10-a bərabər olana qədər davam et
+    }
+}
+
