a5bc49f42644878590be89fca827d4d5b4a1a353
Fatima-QA <camalovafatime2003@gmail.com>
2026-07-24 11:34:56 +0400

    Brauzer-Upcasting

---
+++ src/Brauzer.java
@@ -0,0 +1,11 @@
+public class Brauzer {
+    public void ac() {
+        System.out.println("Brauzer açılır...");
+    }
+}
+
+class Chrome extends Brauzer {
+    public void ac() {
+        System.out.println("Chrome brauzeri açılır, Google hesabı ilə sinxronlaşdırılır...");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Brauzer b = new Chrome();
+        b.ac();
+    }
+}
